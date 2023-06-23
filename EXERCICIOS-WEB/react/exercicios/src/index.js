55484bbe8140b49cf77c8d08000feca3e226c475
IagoDpl <iagodpl@hotmail.com>
2023-06-22 21:31:59 -0300

    Progredindo em mais aulas

---
+++ EXERCICIOS-WEB/react/exercicios/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+import Pai from './componentes/Pai'
+
+
+ReactDom.render( 
+    <div>
+        <Pai nome="Paulo" sobrenome="Silva" />
+    </div>
+, document.getElementById('root'))
